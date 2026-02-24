85dc67648c897ea152f63adb31db494de22747b7
Kai Waldrant <kai.waldrant@outlook.com>
2026-02-24 22:28:25 +0100

    update vitest and add vitest browser mode with playwright

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    // This means we don't have to import `describe`,
+    // `it`, and `expect` into every test file
+    globals: true,
+  },
+});
